e131989bb597d4cad1970c0642baeec3213d3b5c
Aditya kuril <113298694+Adi7890p@users.noreply.github.com>
2025-06-06 20:45:32 +0530

    Update game.js

--- public/game.js
+++ public/game.js
@@ -8,19 +8,23 @@ socket.onopen = () => {
 socket.onmessage = (e) => {
   const data = JSON.parse(e.data);
   if (data.type === 'button') {
-    if (data.value === 'yes') document.getElementById('yesBtn').click();
-    if (data.value === 'no') document.getElementById('noBtn').click();
+    if (data.value === 'yes') {
+  document.getElementById('yesBtn').style.backgroundColor = 'blue';
+  document.getElementById('noBtn').style.backgroundColor = 'white';
+}
+
+if (data.value === 'no') {
+  document.getElementById('yesBtn').style.backgroundColor = 'white';
+  document.getElementById('noBtn').style.backgroundColor = 'blue';
+}
+
   }
 };
 
 document.getElementById('yesBtn').onclick = () => {
   socket.send(JSON.stringify({ type: 'button', roomCode, value: 'yes' }));
-  document.getElementById('yesBtn').style.backgroundColor='blue';
-  document.getElementById('noBtn').style.backgroundColor='white';
 };
 
 document.getElementById('noBtn').onclick = () => {
   socket.send(JSON.stringify({ type: 'button', roomCode, value: 'no' }));
-  document.getElementById('yesBtn').style.backgroundColor='white';
-  document.getElementById('noBtn').style.backgroundColor='blue';
 };
